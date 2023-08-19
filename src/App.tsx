dcd39665f565c0310d1cff50e84c7d6252a2f34b
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-19 19:19:58 -0400

    WIP
    Todo list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import AddItem from './components/add-item';
-import Layout from './layout/layout';
-import { Box, Stack } from '@chakra-ui/react';
+import AddItem from "./components/add-item";
+import ItemList from "./components/item-list";
+import Layout from "./layout/layout";
+import { Box, Stack } from "@chakra-ui/react";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Layout>
         <Stack gap="8">
           <AddItem />
+          <ItemList />
         </Stack>
       </Layout>
     </Box>
--- src/components/item-list.tsx
+++ src/components/item-list.tsx
@@ -1,7 +1,11 @@
-import { Center } from '@chakra-ui/react';
+import { Center, Box } from "@chakra-ui/react";
 
 const ItemList = () => {
-  return <Center textAlign="center"></Center>;
+  return (
+    <Center textAlign="center">
+      <Box backgroundColor="white" boxShadow="xl" w={500} h={150}></Box>
+    </Center>
+  );
 };
 
 export default ItemList;
