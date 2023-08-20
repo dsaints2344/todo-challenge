d352a5670c7f4b789f178de3daf7f272974bfa3e
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-19 20:35:05 -0400

    Connecting components to todo store

--- src/components/add-item.tsx
+++ src/components/add-item.tsx
@@ -5,19 +5,37 @@ import {
   InputRightElement,
   Button,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import { todoStore } from "../stores/todo-store";
 
 const AddItem = () => {
+  const [newTodo, setNewTodo] = useState<string>("");
+  const { addTodo } = todoStore();
+
+  const handleNewTodo = () => {
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
   return (
     <Center textAlign="center" me="3%" ms="3%">
       <InputGroup variant="filled" rounded="base" bgColor="white">
         <Input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
           h="55"
           variant="filled"
           placeholder="Create a new todo..."
           focusBorderColor="white"
         />
         <InputRightElement h="55" me="5">
-          <Button size="sm" colorScheme="blue" variant="solid">
+          <Button
+            onClick={handleNewTodo}
+            isDisabled={newTodo.length < 1}
+            size="sm"
+            colorScheme="blue"
+            variant="solid"
+          >
             Add
           </Button>
         </InputRightElement>
--- src/components/item-list.tsx
+++ src/components/item-list.tsx
@@ -1,9 +1,20 @@
-import { Center, Box } from "@chakra-ui/react";
+import { Center, Box, Text } from "@chakra-ui/react";
+import { todoStore } from "../stores/todo-store";
 
 const ItemList = () => {
+  const { todos } = todoStore();
+
   return (
     <Center textAlign="center">
-      <Box backgroundColor="white" boxShadow="xl" w={500} h={150}></Box>
+      <Box backgroundColor="white" boxShadow="xl" w={500} h={150}>
+        {todos.map((t) => {
+          return (
+            <>
+              <Text>{t.description}</Text>
+            </>
+          );
+        })}
+      </Box>
     </Center>
   );
 };
